fc7256d5c3651693bbe5f94a33dd4f2737681c40
lilachdavis <lilacd@gmail.com>
2023-09-22 11:33:45 +0100

    small typo corrected on line 5 of test_todo_list_integration.py

--- TD_Multi_class_program/tests/test_todo_list_integration.py
+++ TD_Multi_class_program/tests/test_todo_list_integration.py
@@ -2,7 +2,7 @@ from lib.todo_class import *
 from lib.todo_list import *
 
 
-def test_incomplete_returns_list_of__incomplete_tasks():
+def test_incomplete_returns_list_of_incomplete_tasks():
     todo_list = TodoList()
     todo1 = Todo('water the dog')
     todo2 = Todo('take the cat for a walk')
